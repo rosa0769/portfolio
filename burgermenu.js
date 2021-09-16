b07b68081894a107db975f9afd2e778a1fbaf5d5
rosa0769 <rosa.sahlholt@hotmail.com>
2021-09-16 12:26:26 +0200

    .

---
+++ burgermenu.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+  console.log("sidenVises");
+  document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+}
+
+function toggleMenu() {
+  console.log("toggleMenu");
+
+  // Tilføjer/fjerner hidden fra burgermenu
+  document.querySelector("#menu").classList.toggle("hidden");
+
+  //Variabel
+  let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+
+  if (erSkjult == true) {
+    document.querySelector("#menuknap").textContent = "☰";
+  } else {
+    document.querySelector("#menuknap").textContent = "✕";
+  }
+}
